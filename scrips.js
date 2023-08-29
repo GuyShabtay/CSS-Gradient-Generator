05fa5e70a780e2aafb1340ba8f30bc04e3416ac7
Guy Shabtay <guysh16@ac.sce.ac.il>
2023-08-29 19:52:06 +0300

    finish the slider

--- scrips.js
+++ scrips.js
@@ -1,8 +1,11 @@
 const slider = document.getElementById('radiusSlider');
-const image = document.getElementById('imageWithBorder');
+const gradientImage = document.querySelector('.gradient-image');
+const result = document.getElementById('result');
 
 slider.addEventListener('input', function() {
   const sliderValue = slider.value;
   const borderRadiusValue = sliderValue + 'px';
-  image.style.borderRadius = borderRadiusValue;
+  gradientImage.style.borderRadius = borderRadiusValue;
+  result.textContent = sliderValue;
+
 });
