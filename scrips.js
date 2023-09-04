b9483f856ecfa7cf839f2d3c6bf105ec75b088dd
Guy Shabtay <guysh16@ac.sce.ac.il>
2023-09-04 20:33:07 +0300

    adding map to circle degrees

--- scrips.js
+++ scrips.js
@@ -26,14 +26,17 @@ const context = canvas.getContext('2d');
 const bg = document.getElementById('bg');
 const screenWidth = window.innerWidth;
 
-
 // canvas.width = gradientImage.innerWidth;
 // canvas.height = gradientImage.innerHeight;
 // canvas.borderRadius = gradientImage.borderRadius;
 
-
-
-let currentDirection, currentPattern, currentShape, color1, color2, theme='light',copiedText;
+let currentDirection,
+  currentPattern,
+  currentShape,
+  color1,
+  color2,
+  theme = 'light',
+  copiedText;
 
 slider.addEventListener('input', function () {
   const sliderValue = slider.value;
@@ -45,25 +48,28 @@ copyButton.addEventListener('click', function () {
   copyToClipBoard();
   // copyToClipBoard.innerText.style.add('removed');
   // copyToClipBoard.innerText.style.remove('removed');
-  copyButton.innerHTML='copied! <i class="fa-solid fa-check"></i>';
+  copyButton.innerHTML = 'copied! <i class="fa-solid fa-check"></i>';
   setTimeout(() => {
-    copyButton.innerHTML='Copy code <i class="fa-regular fa-clipboard"></i>';
+    copyButton.innerHTML = 'Copy code <i class="fa-regular fa-clipboard"></i>';
   }, 3000); // 3000 milliseconds (3 seconds)
- 
 });
 
 const copyToClipBoard = function () {
-  navigator.clipboard.writeText(`background: ${gradientImage.style.background};`);
-  
+  navigator.clipboard.writeText(
+    `background: ${gradientImage.style.background};`
+  );
 };
 const changeGradient = function () {
   console.log(currentDirection, currentPattern);
   if (currentPattern === 'linear')
-  gradientImage.style.background = `${currentPattern}-gradient(${currentDirection}, ${color1}, ${color2})`;
+    gradientImage.style.background = `${currentPattern}-gradient(${currentDirection}, ${color1}, ${color2})`;
   else if (currentPattern === 'radial') {
-    gradientImage.style.background = `${currentPattern}-gradient(at center, ${color1}, ${color2})`;
+    const degreeToPosition = { '0deg': 'top', '45deg': 'right top', '90deg': 'right', '135deg': 'right bottom', '180deg': 'bottom',
+      '225deg': 'left bottom', '270deg': 'left', '315deg': 'left top'};
+      
+    console.log(`${currentPattern}-gradient(circle at ${current}, ${color1}, ${color2})`) ;
   } else
-  gradientImage.style.background = `${currentPattern}-gradient(from ${currentDirection}, ${color1}, ${color2})`;
+    gradientImage.style.background = `${currentPattern}-gradient(from ${currentDirection}, ${color1}, ${color2})`;
 
   console.log(
     (gradientImage.style.background = `${currentPattern}-gradient(${currentDirection}, ${color1}, ${color2})`)
@@ -71,7 +77,7 @@ const changeGradient = function () {
 };
 
 const selectButton = function (buttons, pressedButton) {
-      console.log(` selectButton ${theme}-pressed`);
+  console.log(` selectButton ${theme}-pressed`);
 
   if (!pressedButton.classList.contains(`${theme}-pressed`)) {
     buttons.forEach((button) => {
@@ -79,18 +85,16 @@ const selectButton = function (buttons, pressedButton) {
     });
     pressedButton.classList.add(`${theme}-pressed`);
     return false;
-  } else
-  {
+  } else {
     // pressedButton.classList.add(`${theme}-pressed`);
     // console.log(` color change ${theme}-pressed`);
     return true;
   }
 };
-const selectButtonThemeChange = function (button,changedFrom) {
-      button.classList.remove(`${changedFrom}-pressed`);
-      button.classList.add(`${theme}-pressed`);
+const selectButtonThemeChange = function (button, changedFrom) {
+  button.classList.remove(`${changedFrom}-pressed`);
+  button.classList.add(`${theme}-pressed`);
 };
-
 
 patternButtons.forEach((button) => {
   button.addEventListener('click', () => {
@@ -191,51 +195,49 @@ const init = function () {
 };
 
 switchButton.addEventListener('click', function () {
-  let otherTheme,color;
-  if(theme==='light'){
-    theme='dark';
-    otherTheme='light';
-color='white';
-  }else{
-    theme='light';
-    otherTheme='dark'
-    color='#333'
+  let otherTheme, color;
+  if (theme === 'light') {
+    theme = 'dark';
+    otherTheme = 'light';
+    color = 'white';
+  } else {
+    theme = 'light';
+    otherTheme = 'dark';
+    color = '#333';
   }
-  console.log(theme,otherTheme)
-footer.classList.toggle('light-pressed');
-bodyBG.classList.toggle('dark-bg');
-bodyBG.classList.toggle('dark-color');
-headingText.classList.toggle('dark-color');
-lightIcon.classList.toggle('removed');
-nightIcon.classList.toggle('removed');
-logoImage.src=`${otherTheme}.png`;
-links.forEach((button) => {
-  button.style.color=color;
-  // button.classList.toggle('light-color');
-});
-allButtons.forEach((button) => {
-   button.classList.toggle('dark-btn');
+  console.log(theme, otherTheme);
+  footer.classList.toggle('light-pressed');
+  bodyBG.classList.toggle('dark-bg');
+  bodyBG.classList.toggle('dark-color');
+  headingText.classList.toggle('dark-color');
+  lightIcon.classList.toggle('removed');
+  nightIcon.classList.toggle('removed');
+  logoImage.src = `${otherTheme}.png`;
+  links.forEach((button) => {
+    button.style.color = color;
+    // button.classList.toggle('light-color');
+  });
+  allButtons.forEach((button) => {
+    button.classList.toggle('dark-btn');
 
-  if (button.classList.contains(`${otherTheme}-pressed`))
-  {
-  button.classList.remove(`${otherTheme}-pressed`);
-  button.classList.add(`${theme}-pressed`);
-  }
-});
+    if (button.classList.contains(`${otherTheme}-pressed`)) {
+      button.classList.remove(`${otherTheme}-pressed`);
+      button.classList.add(`${theme}-pressed`);
+    }
+  });
 });
 
 if (screenWidth <= 768) {
   bodyBG.classList.toggle('dark-color');
-//   allButtons.forEach((button) => {
-//     button.classList.toggle('dark-btn');
- 
-//    if (button.classList.contains(`dark-pressed`))
-//    {
-//    button.classList.remove(`dark-pressed`);
-//    button.classList.add(`light-pressed`);
-//    }
-//  });
+  //   allButtons.forEach((button) => {
+  //     button.classList.toggle('dark-btn');
 
+  //    if (button.classList.contains(`dark-pressed`))
+  //    {
+  //    button.classList.remove(`dark-pressed`);
+  //    button.classList.add(`light-pressed`);
+  //    }
+  //  });
 }
 
 function downloadImage() {
@@ -247,11 +249,16 @@ function downloadImage() {
   // img.onerror = function () {
   //   console.error('Image failed to load.');
   // };
-  
+
   gradientImage.onload = function () {
     // Draw the linear background onto the canvas
-    context.drawImage(gradientImage.innerHTML, 0, 0, canvas.width, canvas.height);
-    
+    context.drawImage(
+      gradientImage.innerHTML,
+      0,
+      0,
+      canvas.width,
+      canvas.height
+    );
 
     // Create a data URL and download link after the image has loaded
     const dataURL = canvas.toDataURL('image/png');
@@ -271,12 +278,6 @@ downloadButton.addEventListener('click', () => {
   downloadImage();
 });
 
-
-
-
-
-
-
 function drawRectangle() {
   context.fillStyle = 'blue';
   // context.drawImage(gradientImage,0, 0, 3000, 1500, [40]);
@@ -284,5 +285,3 @@ function drawRectangle() {
 
   context.fill();
 }
-
-
